2880549754c89a66de6e56dc15c65df634bbade0
ashikwebfix <ashik.webfix@gmail.com>
2026-08-22 15:59:51 +0600

    chore: add aaPanel configuration and env files for production

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: 'kinaboo-backend',
+      script: './server/server.js',
+      cwd: './server',
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 6710,
+        DB_HOST: 'localhost',
+        DB_USER: 'kinabodb',
+        DB_PASS: 'k27Hj58XTa7mebJM',
+        DB_NAME: 'kinabodb',
+        SITE_URL: 'https://kinaboo.com'
+      },
+    },
+    {
+      name: 'kinaboo-frontend',
+      script: 'npm',
+      args: 'run start',
+      cwd: './frontend',
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 6711
+      },
+    }
+  ],
+};
