00c0b357d5216c6c5fd4c330f7fb7a3b9872b256
ashikwebfix <ashik.webfix@gmail.com>
2026-09-15 16:02:47 +0600

    chore: remove temporary patch scripts

--- check_db.js
+++
@@ -1,6 +0,0 @@
-const { sequelize } = require('./server/config/db');
-async function check() {
-  const [results] = await sequelize.query("SELECT TABLE_NAME, index_name, count(*) as c FROM information_schema.statistics WHERE table_schema = 'ecommerce' GROUP BY TABLE_NAME, index_name");
-  console.log(results);
-}
-check();
--- test_transfer4.js
+++
@@ -1,9 +0,0 @@
-const { sequelize } = require('./server/config/db');
-const Product = require('./server/models/Product');
-
-async function test() {
-  const p = await Product.findByPk('a015b6f5-65f8-4c2a-8cc0-4722b8d835af');
-  console.log(p ? 'Product exists' : 'Product does not exist');
-  process.exit();
-}
-test();
